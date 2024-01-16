a2136b88dd1a26d9a322ece731cc83d15422bb34
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2024-01-15 21:34:40 -0400

    changed homepage styling

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import test
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('test/', test, name='test'),
     path('error/', views.error, name='error'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
+def test(request):
+    return render(request, 'test.html')
+
+
 def error(request):
     return render(request, 'error.html')
